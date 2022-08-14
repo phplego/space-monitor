ec1bd59ae2dfd4b50f33ec43997660837e46575d
PHPLego <phplego@gmail.com>
2022-08-14 15:56:23 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/fatih/color1"
+	"github.com/fatih/color"
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"golang.org/x/sys/unix"

9d89ae7e05ee86f3381b7e2e57db0d9cb46dca48
phplego <phplego@gmail.com>
2022-08-14 04:23:23 +0300

    simple directory sizes, config, free space

---
+++ go.mod
@@ -0,0 +1,18 @@
+module space-monitor
+
+go 1.19
+
+require (
+	github.com/fatih/color v1.13.0
+	github.com/ilyakaznacheev/cleanenv v1.3.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/ilyakaznacheev/cleanenv"
+	"golang.org/x/sys/unix"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+// Config is a application configuration structure
+type Config struct {
+	Dirs []string `yaml:"dirs"`
+}
+
+type DirInfoStruct struct {
+	size  int64
+	files int
+	dirs  int
+}
+
+func DirSize(path string) (DirInfoStruct, error) {
+	var info DirInfoStruct = DirInfoStruct{}
+	err := filepath.Walk(path, func(path string, fileInfo os.FileInfo, err error) error {
+		if err != nil {
+			//err = fmt.Errorf("WalkError: %w", err)
+			//color.HiYellow(err.Error())
+			//return err
+		} else {
+			//fmt.Println(path)
+			if !fileInfo.IsDir() {
+				info.files++
+				info.size += fileInfo.Size()
+			} else {
+				info.dirs++
+			}
+		}
+		return nil
+	})
+	return info, err
+}
+
+func HumanSize(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB", float64(b)/float64(div), "KMGTPE"[exp])
+}
+
+func GetFreeSpace() (int64, error) {
+	wd, err := os.Getwd()
+	var stat unix.Statfs_t
+
+	err = unix.Statfs(wd, &stat)
+	if err != nil {
+		color.HiRed(err.Error())
+		return 0, err
+	}
+
+	// Available blocks * size per block = available space in bytes
+	return int64(stat.Bavail) * stat.Bsize, nil
+}
+
+func main() {
+	var cfg Config
+	err := cleanenv.ReadConfig("config.yaml", &cfg)
+	if err != nil {
+		color.HiRed(err.Error())
+		return
+	}
+
+	// for each directory
+	for _, path := range cfg.Dirs {
+		dirInfo, err := DirSize(path)
+		if err != nil {
+			color.HiRed(err.Error())
+			//continue
+		}
+		fmt.Println("size of " + path + ": " + HumanSize(dirInfo.size) +
+			" files: " + strconv.Itoa(dirInfo.files) +
+			" dirs: " + strconv.Itoa(dirInfo.dirs))
+	}
+
+	// calculate free space
+	var space, _ = GetFreeSpace()
+	fmt.Println("Free space:", HumanSize(space))
+}

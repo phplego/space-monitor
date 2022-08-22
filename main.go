8df0e3c59fccc8730d738a3f5da3cd2f65a801f4
phplego <phplego@gmail.com>
2022-08-23 01:34:51 +0300

    snapshot as directory, max-snapshots config option, free space delta

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ import (
 	"golang.org/x/sys/unix"
 	"gopkg.in/natefinch/lumberjack.v2"
 	"gopkg.in/yaml.v2"
+	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -34,7 +37,8 @@ var (
 
 // Config is an application configuration structure
 type Config struct {
-	Dirs []Config_DirectorySettings `yaml:"dirs"`
+	Dirs         []Config_DirectorySettings `yaml:"dirs"`
+	MaxSnapshots int                        `yaml:"max-snapshots"`
 }
 
 type Config_DirectorySettings struct {
@@ -50,6 +54,10 @@ type DirInfoStruct struct {
 	StartTime time.Time `yaml:"stime"` // the time when the scan was started
 }
 
+type SnapshotStruct struct {
+	FreeSpace int64 `yaml:"free-space"`
+}
+
 func ColorHeader(str string, a ...interface{}) string {
 	return color.New(color.FgBlue).Add(color.Bold).Sprintf(str, a...)
 }
@@ -62,11 +70,16 @@ func ColorPale(str string, a ...interface{}) string {
 	return color.New(color.FgHiBlack).Add(color.Bold).Sprintf(str, a...)
 }
 
+func LogErr(v ...any) {
+	gLogger.Println(v...)
+	fmt.Println(v...)
+}
+
 func InitLogger() {
 	var logFilename = GetAppDir() + "/space-monitor.log"
 	file, err := os.OpenFile(logFilename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Printf("error opening file: %v", err)
+		LogErr("error opening file: %v", err)
 		os.Exit(1)
 	}
 	gLogger = *log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
@@ -79,11 +92,18 @@ func InitLogger() {
 }
 
 func InitConfig() {
+	// default config values
+	gCfg.MaxSnapshots = 20
 	err := cleanenv.ReadConfig(GetAppDir()+"/config.yaml", &gCfg)
 	if err != nil {
 		color.HiRed(err.Error())
 		return
 	}
+}
+
+func InitDataDirs() {
+	os.Mkdir(gDataDir, 0777)
+	os.Mkdir(gDataDir+"/"+gStartTime.Format("2006-01-02 15:04:05"), 0777)
 }
 
 func GetAppDir() string {
@@ -98,39 +118,37 @@ func GetHash(text string) string {
 	return hex.EncodeToString(hash[:])
 }
 
+func SaveDirInfo(path string, dirInfo DirInfoStruct) {
+	pathHash := GetHash(path)
+	dirInfoFilePath := fmt.Sprintf(gDataDir+"/%s/dirinfo-%s.dat", gStartTime.Format("2006-01-02 15:04:05"), pathHash)
+	dirInfoFile, err := os.OpenFile(dirInfoFilePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		LogErr("error opening file: %v", err)
+		os.Exit(1)
+	}
+	bytes, _ := yaml.Marshal(dirInfo)
+	dirInfoFile.WriteString(string(bytes))
+}
+
 func LoadPrevDirInfo(dir string, stepsBack int) DirInfoStruct {
-	files, _ := filepath.Glob(gDataDir + fmt.Sprintf("/snapshot-*-%s.dat", GetHash(dir)))
+	files, _ := filepath.Glob(gDataDir + fmt.Sprintf("/*/dirinfo-%s.dat", GetHash(dir)))
 	if files == nil {
-		fmt.Printf("no snapshot files for %s\n", color.BlueString(dir))
+		LogErr("no dirinfo files for %s\n", color.BlueString(dir))
 		return DirInfoStruct{}
 	}
 	sort.Strings(files)
 	index := len(files) - 1 - stepsBack
 	if index < 0 || index >= len(files) {
-		fmt.Println("Error: out of bounds snapshot array. index=" + strconv.Itoa(index))
+		LogErr("Error: out of bounds dirinfo array. index=" + strconv.Itoa(index))
 	}
 	last := files[index]
 	bytes, _ := os.ReadFile(last)
 	info := DirInfoStruct{}
 	err := yaml.Unmarshal(bytes, &info)
 	if err != nil {
-		color.HiRed(err.Error())
-		return DirInfoStruct{}
+		LogErr(err.Error())
 	}
 	return info
-}
-
-func SaveDirInfo(path string, dirInfo DirInfoStruct) {
-	pathHash := GetHash(path)
-	os.Mkdir(gDataDir, 0777)
-	snapshotFilePath := fmt.Sprintf(gDataDir+"/snapshot-%s-%s.dat", gStartTime.Format("2006-01-02 15:04:05"), pathHash)
-	snapshotFile, err := os.OpenFile(snapshotFilePath, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		fmt.Printf("error opening file: %v", err)
-		os.Exit(1)
-	}
-	bytes, _ := yaml.Marshal(dirInfo)
-	snapshotFile.WriteString(string(bytes))
 }
 
 func ProcessDirectory(path string) (DirInfoStruct, error) {
@@ -186,12 +204,68 @@ func GetFreeSpace() (int64, error) {
 
 	err = unix.Statfs(wd, &stat)
 	if err != nil {
-		color.HiRed(err.Error())
+		LogErr(err.Error())
 		return 0, err
 	}
 
 	// Available blocks * Size per block = available space in bytes
 	return int64(stat.Bavail) * int64(stat.Bsize), nil
+}
+
+func SaveSnapshot(snapshot SnapshotStruct) {
+	snapshotFilePath := fmt.Sprintf(gDataDir+"/%s/snapshot.dat", gStartTime.Format("2006-01-02 15:04:05"))
+	snapshotFile, err := os.OpenFile(snapshotFilePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		LogErr("error opening snapshot file: %v", err)
+		os.Exit(1)
+	}
+	bytes, _ := yaml.Marshal(snapshot)
+	snapshotFile.WriteString(string(bytes))
+}
+
+func LoadPrevSnapshot(stepsBack int) SnapshotStruct {
+	files, _ := filepath.Glob(gDataDir + fmt.Sprintf("/*/snapshot.dat"))
+	if files == nil {
+		LogErr("no snapshot files")
+		return SnapshotStruct{}
+	}
+	sort.Strings(files)
+	index := len(files) - 1 - stepsBack
+	if index < 0 || index >= len(files) {
+		LogErr("Error: out of bounds snapshot array. index=" + strconv.Itoa(index))
+	}
+	last := files[index]
+	bytes, _ := os.ReadFile(last)
+	snap := SnapshotStruct{}
+	err := yaml.Unmarshal(bytes, &snap)
+	if err != nil {
+		color.HiRed(err.Error())
+	}
+	return snap
+}
+
+func DeleteOldSnapshots() {
+	files, _ := ioutil.ReadDir(gDataDir)
+	var dirs []fs.FileInfo
+	for _, file := range files {
+		if file.IsDir() {
+			dirs = append(dirs, file)
+		}
+	}
+	if len(dirs) <= gCfg.MaxSnapshots {
+		return // no need to delete
+	}
+	sort.Slice(dirs, func(i, j int) bool { // sort dirs (older first)
+		return strings.Compare(dirs[i].Name(), dirs[j].Name()) < 0
+	})
+
+	for _, dir := range dirs[0 : len(dirs)-gCfg.MaxSnapshots] {
+		err := os.RemoveAll(gDataDir + "/" + dir.Name())
+		if err != nil {
+			LogErr(err.Error())
+			return
+		}
+	}
 }
 
 func main() {
@@ -200,6 +274,7 @@ func main() {
 
 	InitLogger()
 	InitConfig()
+	InitDataDirs()
 
 	tableWriter := table.NewWriter()
 	tableWriter.SetStyle(table.StyleRounded)
@@ -215,7 +290,7 @@ func main() {
 		start := time.Now()
 		dirInfo, err := ProcessDirectory(dir.Path)
 		if err != nil {
-			color.HiRed(err.Error())
+			LogErr(err.Error())
 			//continue
 		}
 
@@ -223,11 +298,11 @@ func main() {
 
 		deltaSize := ""
 		if prevDirInfo.Size != 0 && prevDirInfo.Size != dirInfo.Size {
-			if dirInfo.Size >= prevDirInfo.Size {
-				deltaSize = " (+" + HumanSize(dirInfo.Size-prevDirInfo.Size) + ")"
-			} else {
-				deltaSize = " (" + HumanSize(dirInfo.Size-prevDirInfo.Size) + ")"
+			var sign = "+"
+			if dirInfo.Size < prevDirInfo.Size {
+				sign = ""
 			}
+			deltaSize = " (" + sign + HumanSize(dirInfo.Size-prevDirInfo.Size) + ")"
 		}
 
 		deltaDirs := ""
@@ -257,11 +332,25 @@ func main() {
 	tableWriter.AppendRow(table.Row{ColorHeaderHi("start time"), gStartTime.Format(time.RFC822)})
 	tableWriter.AppendSeparator()
 
+	prevSnapshot := LoadPrevSnapshot(0)
+
 	// calculate free space
 	var space, _ = GetFreeSpace()
+	SaveSnapshot(SnapshotStruct{space})
 
-	tableWriter.AppendRow(table.Row{"FREE SPACE", color.HiGreenString(HumanSize(space)), "", "", "", time.Since(gStartTime).Round(time.Millisecond)})
+	deltaFreeSpace := ""
+	if prevSnapshot.FreeSpace != 0 && prevSnapshot.FreeSpace != space {
+		var sign = "+"
+		if space < prevSnapshot.FreeSpace {
+			sign = ""
+		}
+		deltaFreeSpace = " (" + sign + HumanSize(space-prevSnapshot.FreeSpace) + ")"
+	}
+
+	tableWriter.AppendRow(table.Row{"FREE SPACE", color.HiGreenString(HumanSize(space)) + deltaFreeSpace, "", "", "", time.Since(gStartTime).Round(time.Millisecond)})
 	tableWriter.Render()
+
+	DeleteOldSnapshots()
 
 	if *gDaemonMode {
 		fmt.Println("Running in daemon mode..")

ca00c4ca74607175b6a74a7cb15a025f568ade18
phplego <phplego@gmail.com>
2022-09-10 04:06:12 +0300

    add git commit and push script

---
+++ gum_commit_and_push.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# COMMIT
+# ------
+if [[ `git status --porcelain` ]]; then
+  # Changes
+  LIST=$(git status --porcelain)
+
+  gum style \
+  	--border-foreground 212 --border double \
+  	 --margin "1 2" --padding "1 4" \
+  	"CHANGES:" "" "$LIST"
+
+  gum confirm "Add these files?" || exit
+
+  git add . # ADD EVERYTHING!
+
+  DESC=$(gum input --placeholder "Details of this change [ENTER to finish]") || exit
+
+  gum confirm "Commit changes with message '$DESC'? " || exit
+  git commit -m "$DESC"
+
+else
+  # No changes
+  echo "No local changes."
+fi
+
+# PUSH
+# ----
+if [[ `git diff --stat origin/main` ]]; then
+  # Unpushed commits
+  git diff --stat @{upstream}
+  gum confirm "Unpushed commits found. Do you want to push them?" || exit
+
+  git push || echo "Push failed!" && exit
+  echo "Pushed successfully"
+else
+  # No Unpushed commits
+  echo "No diff with remote. Everything up to date. Exit"
+fi
+
+
